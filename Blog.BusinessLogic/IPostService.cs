244621a59261edc4a2359f29f72f712522d142ed
Alastair Smith <github@alastairsmith.me.uk>
2012-03-12 21:58:42 +0000

    Add skeleton IPostService interface.

---
+++ Blog.BusinessLogic/IPostService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Blog.BusinessLogic.Models;
+
+namespace Blog.BusinessLogic
+{
+    public interface IPostService
+    {
+        void Save(Post post);
+        IEnumerable<Post> GetPosts();
+        IEnumerable<Post> GetTopPosts();
+    }
+}
