0d76c22bf58a38e602f72ba013b3e49f55da5564
Alastair Smith <github@alastairsmith.me.uk>
2012-03-12 22:02:46 +0000

    Add skeleton PostService implementation of IPostService. Currently throws a NotImplementedException on all operations.

---
+++ Blog.BusinessLogic/Implementation/PostService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Blog.BusinessLogic.Models;
+
+namespace Blog.BusinessLogic.Implementation
+{
+    public class PostService : IPostService
+    {
+        public void SavePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetTopPosts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
