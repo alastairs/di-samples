587c970c414695c4f4ea80f5f20ed1e25fef6f9d
Alastair Smith <github@alastairsmith.me.uk>
2012-03-20 18:18:02 +0000

    Add beginnings of Simple.Data-based DataAccess classes.

---
+++ Blog.DataAccess/DataModels/Posts.cs
@@ -0,0 +1,9 @@
+﻿using Simple.Data;
+
+namespace Blog.DataAccess.DataModels
+{
+    class Posts
+    {
+        private readonly dynamic db = Database.OpenFile(@"Database\BlogDatabase.sdf");
+    }
+}
