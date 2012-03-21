d1d2f7e677f3af429e888dc395a4d92f52898e91
Alastair Smith <github@alastairsmith.me.uk>
2012-03-21 11:24:58 +0000

    Add a basic find by identifier method for Posts.

--- Blog.DataAccess/DataModels/Posts.cs
+++ Blog.DataAccess/DataModels/Posts.cs
@@ -1,9 +1,16 @@
-﻿using Simple.Data;
+﻿using Blog.BusinessLogic.Models;
+using Simple.Data;
 
 namespace Blog.DataAccess.DataModels
 {
     class Posts
     {
         private readonly dynamic db = Database.OpenFile(@"Database\BlogDatabase.sdf");
+
+        public Post GetById(int id)
+        {
+            var databasePost = db.Posts.FindById(id);
+            return new Post();
+        }
     }
 }
