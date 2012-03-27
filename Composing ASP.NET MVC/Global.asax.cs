1e376ae6cec982815b45d3b7d849325defcfd315
Alastair Smith <github@alastairsmith.me.uk>
2012-03-27 20:53:38 +0100

    Fix default route.

--- Composing ASP.NET MVC/Global.asax.cs
+++ Composing ASP.NET MVC/Global.asax.cs
@@ -20,7 +20,7 @@ namespace Composing_ASP.NET_MVC
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Post", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
