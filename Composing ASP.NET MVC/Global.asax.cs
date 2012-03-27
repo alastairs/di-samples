90d4e0c4713385b9792196d634deb75011da439b
Alastair Smith <github@alastairsmith.me.uk>
2012-03-27 20:33:27 +0100

    Tidy up usings.

--- Composing ASP.NET MVC/Global.asax.cs
+++ Composing ASP.NET MVC/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.SessionState;
 
 namespace Composing_ASP.NET_MVC
 {
