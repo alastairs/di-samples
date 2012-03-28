65c3ad3a5be57ad3a3d0b9fb77197283a434d7fc
Alastair Smith <github@alastairsmith.me.uk>
2012-03-28 12:02:27 +0100

    Rename MainWindow to EditorWindow.

---
+++ Composing WPF/Views/EditorWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Composing_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for EditorWindow.xaml
+    /// </summary>
+    public partial class EditorWindow
+    {
+        public EditorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
