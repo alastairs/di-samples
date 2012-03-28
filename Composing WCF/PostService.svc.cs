cac2e229481fe01c4d9b859358a94ab22704cced
Alastair Smith <github@alastairsmith.me.uk>
2012-03-28 17:00:36 +0100

    Add constructor injection to PostService.

--- Composing WCF/PostService.svc.cs
+++ Composing WCF/PostService.svc.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
+using Blog.BusinessLogic;
 
 namespace Composing_WCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     public class PostService : IPostService
     {
+        private readonly IPostRepository postRepository;
+        private readonly IContractMapper contractMapper;
+
+        public PostService(IPostRepository postRepository, IContractMapper contractMapper)
+        {
+            if (postRepository == null)
+            {
+                throw new ArgumentNullException("postRepository");
+            }
+
+            if (contractMapper == null)
+            {
+                throw new ArgumentNullException("contractMapper");
+            }
+
+            this.postRepository = postRepository;
+            this.contractMapper = contractMapper;
+        }
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
