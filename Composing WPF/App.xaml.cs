c3727047bb1526e9249846a4287526e075d838db
Alastair Smith <github@alastairsmith.me.uk>
2012-03-28 10:44:23 +0100

    Implement composition root.

--- Composing WPF/App.xaml.cs
+++ Composing WPF/App.xaml.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
+using Composing_WPF.ViewModels;
+using Ninject;
+using Ninject.Extensions.Conventions;
 
 namespace Composing_WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ComposeApplication();
+        }
+
+        private static void ComposeApplication()
+        {
+            // Ninject
+            var kernel = new StandardKernel();
+
+            // Manual binding:
+            //kernel.Bind<IEditorViewModel>().To<EditorViewModel>();
+
+            //Auto-wiring: bind IFoo to Foo
+            kernel.Bind(x => x.FromAssembliesMatching("*").SelectAllClasses().BindDefaultInterface());
+        }
     }
 }
