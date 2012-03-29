0ac72db2638aedd9c7f15122bf649636cbb6e865
Alastair Smith <github@alastairsmith.me.uk>
2012-03-29 23:06:01 +0100

    Add example of Bastard Injection.

---
+++ AntiPatterns/BastardInjection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Blog.BusinessLogic;
+using Blog.DataAccess;
+
+namespace AntiPatterns
+{
+    class BastardInjection
+    {
+        private readonly IPostRepository postRepository;
+
+        public BastardInjection(IPostRepository postRepository)
+        {
+            if (postRepository == null)
+            {
+                throw new ArgumentNullException("postRepository");
+            }
+
+            this.postRepository = postRepository;
+        }
+
+        // This is bastard injection as the default implementation is not a local default: 
+        // it's coming from the Data Access layer.
+        public BastardInjection() : this(new PostRepository()) { }
+
+        public void Foo()
+        {
+            postRepository.GetAllPosts();
+        }
+    }
+}
