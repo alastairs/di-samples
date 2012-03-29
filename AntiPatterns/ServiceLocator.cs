bbb1e90ec7872951d234444a4498a3f9160e076c
Alastair Smith <github@alastairsmith.me.uk>
2012-03-29 23:11:51 +0100

    Add example of Service Locator.

---
+++ AntiPatterns/ServiceLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Blog.BusinessLogic;
+using Ninject;
+
+namespace AntiPatterns
+{
+    class ServiceLocator
+    {
+        private readonly IKernel kernel;
+
+        public ServiceLocator(IKernel kernel)
+        {
+            if (kernel == null)
+            {
+                throw new ArgumentNullException("kernel");
+            }
+
+            this.kernel = kernel;
+        }
+
+        public void Foo()
+        {
+            var postRepository = kernel.Get<IPostRepository>();
+            postRepository.GetAllPosts();
+        }
+    }
+}
