693b5a66025aa206857a55e711faff48e4e57bfd
Alastair Smith <github@alastairsmith.me.uk>
2012-03-29 23:05:35 +0100

    Add AntiPatterns project and an example of Control Freak.

---
+++ AntiPatterns/ControlFreak.cs
@@ -0,0 +1,21 @@
+﻿using Blog.BusinessLogic;
+using Blog.DataAccess;
+
+namespace AntiPatterns
+{
+    public class ControlFreak
+    {
+        private readonly IPostRepository postRepository;
+
+        public ControlFreak()
+        {
+            // Directly instantiating a dependency here is an example of being a Control Freak
+            postRepository = new PostRepository();
+        }
+
+        public void Foo()
+        {
+            postRepository.GetAllPosts();
+        }
+    }
+}
