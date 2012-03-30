f2bfdd6367fbedf675abc61e576e9966f364663f
Alastair Smith <github@alastairsmith.me.uk>
2012-03-30 18:20:57 +0100

    Make DefaultDateTimeContext public so it can be bound.

--- Blog.BusinessLogic/DefaultDateTimeContext.cs
+++ Blog.BusinessLogic/DefaultDateTimeContext.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Blog.BusinessLogic
 {
-    internal class DefaultDateTimeContext : DateTimeContext
+    public class DefaultDateTimeContext : DateTimeContext
     {
         public override DateTime Now
         {
