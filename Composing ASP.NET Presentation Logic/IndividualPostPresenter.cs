8634334ae347ded3e621ee3d43c6cfb8a485da41
Alastair Smith <github@alastairsmith.me.uk>
2012-03-30 09:59:00 +0100

    Implement IndividualPostPresenter.

--- Composing ASP.NET Presentation Logic/IndividualPostPresenter.cs
+++ Composing ASP.NET Presentation Logic/IndividualPostPresenter.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Composing_ASP.NET_Presentation_Logic
 {
     public class IndividualPostPresenter
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+        public DateTime PublicationDate { get; set; }
     }
 }
