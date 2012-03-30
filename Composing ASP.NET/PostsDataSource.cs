6b1291633779b60ebc987921aec695c3872709a8
Alastair Smith <github@alastairsmith.me.uk>
2012-03-30 10:03:44 +0100

    Add PostsDataSource.

---
+++ Composing ASP.NET/PostsDataSource.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using Composing_ASP.NET_Presentation_Logic;
+using Ninject;
+
+namespace Composing_ASP.NET
+{
+    public class PostsDataSource
+    {
+        private readonly PostsPresenter presenter;
+
+        public PostsDataSource()
+        {
+            var kernel = (IKernel)HttpContext.Current.Application["container"];
+            presenter = kernel.Get<PostsPresenter>();
+        }
+
+        public IEnumerable<IndividualPostPresenter> SelectAll()
+        {
+            return presenter.SelectAll();
+        }
+
+        public void Update(IndividualPostPresenter post)
+        {
+            presenter.Update(post);
+        }
+    }
+}
