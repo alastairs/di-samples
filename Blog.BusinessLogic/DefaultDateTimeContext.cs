ba00fe36e3743584589e08de79a489d2e7debd9a
Alastair Smith <github@alastairsmith.me.uk>
2012-03-30 18:20:31 +0100

    Implement DefaultDateTimeContext.

--- Blog.BusinessLogic/DefaultDateTimeContext.cs
+++ Blog.BusinessLogic/DefaultDateTimeContext.cs
@@ -1,6 +1,17 @@
+using System;
+
 namespace Blog.BusinessLogic
 {
     internal class DefaultDateTimeContext : DateTimeContext
     {
+        public override DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        public override DateTime UtcNow
+        {
+            get { return DateTime.UtcNow; }
+        }
     }
 }
