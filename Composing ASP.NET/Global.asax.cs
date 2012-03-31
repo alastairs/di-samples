0e09f6ec00dd863f1223d69d8e4c2f42b1f35ee1
Alastair Smith <github@alastairsmith.me.uk>
2012-03-31 20:15:28 +0100

    Wire up the Ambient Context.

--- Composing ASP.NET/Global.asax.cs
+++ Composing ASP.NET/Global.asax.cs
@@ -21,7 +21,23 @@ namespace Composing_ASP.NET
             kernel.Bind<IPostService>().To<PostService>();
             kernel.Bind<IPostRepository>().To<PostRepository>();
             kernel.Bind<IRatingAlgorithm>().To<SimpleAverageRatingAlgorithm>();
+            
+            /* 
+             * Wire up an Ambient Context.
+             * 
+             * Could be achieved by decorating DateTimeContext.Current with [Inject].
+             * You'd then need to bind the type to the required implementation as follows:
+             * 
+             * kernel.Bind<DateTimeContext>().To<DefaultDateTimeContext>();
+             *
+             * Unfortunately, this then means that the BusinessLogic assembly is tied to the
+             * IoC container, as DateTimeContext is defined in that layer. Given how easy it
+             * is to wire this up manually, it's not worth bringing an IoC container into the
+             * picture unless you really need to.
+             */
 
+            DateTimeContext.Current = new DefaultDateTimeContext();
+            
             Application["container"] = kernel;
         }
     }
--- Composing WCF/PostServiceHostFactory.cs
+++ Composing WCF/PostServiceHostFactory.cs
@@ -19,6 +19,22 @@ namespace Composing_WCF
             kernel.Bind<IPostRepository>().To<PostRepository>();
             kernel.Bind<IContractMapper>().To<ContractMapper>();
             kernel.Bind<IPostService>().To<PostService>();
+
+            /* 
+             * Wire up an Ambient Context.
+             * 
+             * Could be achieved by decorating DateTimeContext.Current with [Inject].
+             * You'd then need to bind the type to the required implementation as follows:
+             * 
+             * kernel.Bind<DateTimeContext>().To<DefaultDateTimeContext>();
+             *
+             * Unfortunately, this then means that the BusinessLogic assembly is tied to the
+             * IoC container, as DateTimeContext is defined in that layer. Given how easy it
+             * is to wire this up manually, it's not worth bringing an IoC container into the
+             * picture unless you really need to.
+             */
+
+            DateTimeContext.Current = new DefaultDateTimeContext();
         }
 
         protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
